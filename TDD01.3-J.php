62e497f8498faeeeaa0cbfb1155903f403f6ed0e
khasemsak <45808546+duromatics@users.noreply.github.com>
2018-12-13 14:15:32 +0700

    Add files via upload

--- TDD01.3-J.php
+++ TDD01.3-J.php
@@ -7,11 +7,21 @@
 	$conn = new mysqli($server, $username, $password, $db);
 	mysqli_query($conn, "SET NAMES utf8");
 	
-	$sql_text = "SELECT * FROM tbl_khasemsak_tdd_job WHERE PEA LIKE '%กกค%' ";
-	$query_text = mysqli_query($conn,$sql_text);
-	
-	while($obj = mysqli_fetch_array($query_text))
+	function query($txtin)
 	{
-		echo $obj["PEA"]." ".$obj["WBS"]." ".$obj["Ustatus"]." 
-		".$obj["Sstatus"]." ".$obj["Name"]." ".$obj["Quan"]." ".$obj["ActQuan"]."<br>";
+	$sql_text = "SELECT * FROM tbl_khasemsak_tdd_job WHERE PEA LIKE '%".$txtin."%'";
+	$query = mysqli_query($conn,$sql_text);
+	while($obj_result = mysqli_fetch_array($query))
+	{
+	$result = $result."\n".$obj_result["WBS"].$obj_result["Ustatus"].$obj_result["Status"].$obj_result["Name"].$obj_result["Quan"].$obj_result["ActQuan"];
 	}
+	return $return;
+	}
+	//$sql_text = "SELECT * FROM tbl_khasemsak_tdd_job WHERE PEA LIKE '%กกค%' ";
+	//$query_text = mysqli_query($conn,$sql_text);
+	
+	//while($obj = mysqli_fetch_array($query_text))
+	//{
+		//echo $obj["PEA"]." ".$obj["WBS"]." ".$obj["Ustatus"]." 
+		//".$obj["Sstatus"]." ".$obj["Name"]." ".$obj["Quan"]." ".$obj["ActQuan"]."<br>";
+	//}
