5bd306515865c7b8f35aeead55458c91b8337cfe
khasemsak <45808546+duromatics@users.noreply.github.com>
2018-12-14 12:57:12 +0700

    Add files via upload

--- Bot_Khasemsak.php
+++ Bot_Khasemsak.php
@@ -1,9 +1,11 @@
 <?php
-	require('TDD01.3-J.php');
+	//require('TDD01.3-J.php');
+	require('Render_Khasemsak.php');
 	function send_back($txtin,$replyToken)
 	{
 		$access_token = '2HGhmCyQ9usib5/iCsstrgFuzOQrzk1bioHQDj67T7rZ6jzvgTywHOJGBLFkyXYlaHT84VSrtbZ3kyfr+qs79n/9j/4QlqZgLg3dVZm+xD/q30qzUL+uJsJTrk3JUbNtO/OGP0NBogD81KXGvDFgPAdB04t89/1O/w1cDnyilFU=';
-		$messages = ['type' => 'text','text' => $txtin];
+		//$messages = ['type' => 'text','text' => $txtin];
+		$messages = flex_msg($txtin);//สร้างตัวแปร
 		$url = 'https://api.line.me/v2/bot/message/reply';
 		$data = [
 					'replyToken' => $replyToken,
@@ -35,13 +37,13 @@
 						{	
 							$len_txt = strlen($txtin);
 							$keyword = substr($txtin,1,$len_txt-1);						
-							$result = search($keyword);
+							//$result = search($keyword);
 							/*$len = strlen($result);
 							/*if($len > 1900)
 							{
 							$result = "ข้อความยาวเกินไป  ".$len;
 							}*/
-							send_back($result,$replyToken);
+							send_back($keyword,$replyToken);
 						}
 					}
 				}
