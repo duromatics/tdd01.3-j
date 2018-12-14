6c8194033934754e754d1c73e1404ba779565c5e
khasemsak <45808546+duromatics@users.noreply.github.com>
2018-12-15 00:57:14 +0700

    Add files via upload

--- Result_Khasemsak.php
+++ Result_Khasemsak.php
@@ -54,9 +54,9 @@
 				</div>
 			</div>
 			
-			<div class="container-fluid" style="background-color:#dc9ef7;">
+			<div class="container-fluid" style="background-color:#ebcaf9;">
 				<div class="row">
-					<div class="col-sm2 offset-sm-1">
+					<div class="col-sm1 offset-sm-1">
 						<h5><B>ปริมาณงาน ทั้งโครงการ : </B> 1,408 วงจร - กม.</h5><br>
 						<h5><B>อนุมัติงานแล้ว : </B> 367.65 วงจร - กม.</h5><br>
 						<h5><B>ผลงานสะสม ทั้งโครงการ : </B> 131.36 วงจร - กม.</h5>
